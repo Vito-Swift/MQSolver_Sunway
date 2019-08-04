770a5d671863b2e0dc6223b1d7e0dd961abbe0b3
Vito Wu <117010285@Link.cuhk.edu.cn>
2019-08-04 21:56:57 +0800

    upload Type4_58

---
+++ SolveType4_68/main.cpp
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <bitset>
+#include "../ttmath-0.9.3/ttmath/ttmath.h"
+
+typedef ttmath::Int<1> ttint64;
+typedef ttmath::Int<2> ttint128;
+
+struct term {
+  uint32_t data[3];
+};
+struct poly {
+  term *p;
+  int length;
+};
+
+#define M 68
+#define N 68
+#define LEN 10001
+#define VARIABLE_NUM 10
+#define EQUATION_NUM 23
+#define SEARCH_SPACE 58
+
+void repeatPoly(poly &spoly);
+void ffile2poly(FILE *fr, poly poly[][N + 1], int m);
+void addPoly(poly &dstPoly, poly &poly);
+void addTerm(poly &dstPoly, term &term);
+void loadPD(poly fullpoly[M][N + 1], int64_t pdhi[M][N], int64_t pdlo[M][N]);
+bool verifyPoly(uint32_t guess[N], poly spoly[M][N + 1]);
+void file2poly(FILE *fr, poly spoly[M][N + 1]);
+void checkConsist_23x10(const uint32_t clist[11], uint32_t &mask);
+void extractSolution_23x10(const uint32_t clist[11], uint32_t sol[10]);
+const std::string currentDateTime();
+
+int main() {
+    // MPI Init
+    int size, rank;
+
+    File *fr = fopen("mq-resident", "rb");
+    File *frr = fopen("mq-f", "rb");
+    poly fullpoly[M][N + 1];
+    int64_t pdhi[EQUATION_NUM][N] = {0};
+    int64_t pdlo[EQUATION_NUM][N] = {0};
+
+    return 0;
+}
